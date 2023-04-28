04ee0995ac87c934c4369fa9b7ea4ade0fadf03d
cuon-murahara <104054782+murahararyosuke@users.noreply.github.com>
2023-04-28 18:34:23 +0900

    Revert "step7"

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Task.create!(
-  title: 'あいうえお',
-  body: 'aaaaa',
- )
