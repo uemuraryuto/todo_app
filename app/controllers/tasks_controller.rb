71bef928075abfc763051c67f66f76ffb9529e96
uemuraryuto <ruemura@cuon.co.jp>
2023-05-12 18:42:16 +0900

    step8-2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,39 @@ class TasksController < ApplicationController
   def show
     @task = Task.find_by(id: params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:notice]='タスクの登録が完了しました'
+      redirect_to root_path
+    else
+      flash.now[:alert]='タスクの登録に失敗しました'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def edit
+		@task = Task.find_by(id: params[:id])
+  end
+
+  def update
+		@task = Task.find_by(id: params[:id])
+		if @task.update(task_params)
+			flash[:notice]='タスクの編集が完了しました'
+			redirect_to root_path
+		else
+			flash.now[:alert]='タスクの編集に失敗しました'	
+			render 'edit', status: :unprocessable_entity
+		end	
+  end
+
+    private
+		def task_params
+			params.require(:task).permit(:title, :body)
+		end
 end
